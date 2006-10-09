7c74d22b455491c4a8caf0516f401fd13d740f9f
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-09 03:36:35 +0000

    Adding the start for Premake scripts. They do not work at all yet.

---
+++ src/controls/premake.lua
@@ -0,0 +1,11 @@
+if ( OS == "windows" ) then
+	project.name = "wxFormBuilder 3rd-Party Controls"
+	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
+else
+	project.name = "wxFormBuilder 3rd-Party Controls-unix"
+end
+project.libdir = "lib"
+
+-- Add sdk projects here.
+dopackage( "" )
+dopackage( "" )
