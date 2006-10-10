9140694eaded1381d7963098c0e0db3910facde3
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-10 23:02:48 +0000

    Cleanup of unused vc and C::B projects. Many updates to the premake.lua scripts. They are still not functional, but getting closer. Remoced TODO.txt because of the issue tracker.

--- 3.x/trunk/sdk/premake.lua
+++ 3.x/trunk/sdk/premake.lua
@@ -1,11 +1,8 @@
-if ( OS == "windows" ) then
-	project.name = "wxFormBuilder SDK"
-	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
-else
-	project.name = "wxFormBuilder SDK-unix"
-end
+project.name = "wxFormBuilder SDK"
+project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
+
 project.libdir = "lib"
 
--- Add sdk projects here.
+-- Add sdk projects here. (This needs to match the directory name not the package name.)
 dopackage( "plugin_interface" )
 dopackage( "tinyxml" )
