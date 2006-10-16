1fe99fb345f12862a451cf72d9fd62df2fe3b08e
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-16 17:21:07 +0000

    Added wxconfig.exe. It is a windows version of the Linux equivalent. It doesn't have as many features, but it does a great job at finding out what compiler and linker options I need.

--- src/controls/build/premake.lua
+++ src/controls/build/premake.lua
@@ -1,10 +1,11 @@
-if ( OS == "windows" ) then
-	project.name = "wxFormBuilder 3rd-Party Controls"
-	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
-else
-	project.name = "wxFormBuilder 3rd-Party Controls-unix"
-end
+project.name = "wxFormBuilder 3rd-Party Controls"
+project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
+
 project.libdir = "lib"
+dofile( "../../../wrap_wxconfig.lua" )
+
+wxCfgInit( ".."..wxCFG_PATH_SEP..".."..wxCFG_PATH_SEP..".."..wxCFG_PATH_SEP )
+wxCfgGetWxVersion()
 
 -- Add sdk projects here.
 dopackage( "propgrid" )
