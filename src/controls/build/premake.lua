4df6f187fda690674add0654728e9b721eaba542
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-10-17 03:40:20 +0000

    Moved premake build scripts and helper files. Added Windows version wx-config. Added start of a wx-config wrapper for Premake.

--- src/controls/build/premake.lua
+++ src/controls/build/premake.lua
@@ -2,10 +2,14 @@ project.name = "wxFormBuilder 3rd-Party Controls"
 project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
 
 project.libdir = "lib"
-dofile( "../../../wrap_wxconfig.lua" )
+dofile( "../../../premake/wrap_wxconfig.lua" )
+wxconfigPath = ".." .. wxCFG_PATH_SEP .. ".." .. wxCFG_PATH_SEP .. ".." .. wxCFG_PATH_SEP .. "premake" .. wxCFG_PATH_SEP
+wxCfgInit( wxconfigPath )
 
-wxCfgInit( ".."..wxCFG_PATH_SEP..".."..wxCFG_PATH_SEP..".."..wxCFG_PATH_SEP )
-wxCfgGetWxVersion()
+--Test out functionality
+print( "wxWidgets version = " .. wxCfgGetWxVersion() )
+print( "wxWidgets Libs = " .. wxCfgGetLibs() )
+print( "wxWidgets cflags = " .. wxCfgGetCFlags() )
 
 -- Add sdk projects here.
 dopackage( "propgrid" )
