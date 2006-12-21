70ca4a504cf94eb0bacc58da42fe568777c54a35
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-12-21 22:44:11 +0000

    Switching to Premake build system.

--- plugins/premake.lua
+++ plugins/premake.lua
@@ -1,14 +1,10 @@
-project.name = "wxFormBuilder"
-if ( OS == "windows" ) then
-	project.configs = { "Debug", "Debug (Unicode)", "Release", "Release (Unicode)" }
-end
-
---project.path = "../"
+project.name = "wxFormBuilder Plugins"
 
 -- Add packages here.
-dopackage( "src" )
-dopackage( "sdk/plugin_interface" )
-dopackage( "sdk/tinyxml" )
-dopackage( "src/controls/build/propgrid" )
-dopackage( "src/controls/build/wxFlatNotebook" )
-dopackage( "src/controls/build/wxScintilla" )
+dopackage( "additional" )
+dopackage( "common" )
+dopackage( "layout" )
+
+-- Add dependency packages here.
+dopackage( "../sdk/plugin_interface" )
+dopackage( "../sdk/tinyxml" )
