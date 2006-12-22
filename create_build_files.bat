1f56719f3785a7eb31198d9ebd318256d988b882
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2006-12-22 12:06:13 +0000

    missed build file scripts in merge

--- create_build_files.bat
+++ create_build_files.bat
@@ -1,13 +1,11 @@
 @echo off
 
-echo Genrerating the Code::Blocks projects and workspaces...
-premake\premake-win32.exe --target cb-gcc
+premake\premake-win32.exe --target cb-gcc --unicode --with-wx-shared
 echo done...
 echo.
 
-echo Generating the GNU makefiles...
-premake\premake-win32.exe --target gnu
+premake\premake-win32.exe --target gnu --unicode --with-wx-shared
 echo done...
 echo.
 
-echo Done generating all makefiles for wxFormBuilder.
+echo Done generating all project files for wxFormBuilder.
