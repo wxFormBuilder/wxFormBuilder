99d3924a420c12e63ed330a9fbbd30c2a352d238
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-03-15 23:44:52 +0000

    updated for 2.8.2 and new dir structure

--- support/make_wxfb_archive.sh
+++ support/make_wxfb_archive.sh
@@ -1,10 +1,11 @@
-rm -r ../wxfb
-svn export bin ../wxfb
-cp bin/wxFormBuilder ../wxfb
-cp -R bin/lib ../wxfb
-rm ../wxfb/lib/*d.so
-cp bin/plugins/additional/libadditional.so ../wxfb/plugins/additional
-cp bin/plugins/common/libcommon.so ../wxfb/plugins/common
-cp bin/plugins/layout/liblayout.so ../wxfb/plugins/layout
-cp /opt/wx/2.8/lib/libwx_gtk2u-2.8.so.0 ../wxfb/lib
-cd ..
+rm -r ../../wxfb
+svn export ../bin ../../wxfb
+cp ../bin/wxFormBuilder ../../wxfb
+cp -R ../bin/lib ../../wxfb
+rm ../../wxfb/lib/*d.so
+cp ../bin/plugins/additional/libadditional.so ../../wxfb/plugins/additional
+cp ../bin/plugins/common/libcommon.so ../../wxfb/plugins/common
+cp ../bin/plugins/layout/liblayout.so ../../wxfb/plugins/layout
+cp ../bin/plugins/wxAdditions/libwxadditions\-mini.so ../../wxfb/plugins/wxAdditions
+cp /opt/wx/2.8.2/lib/libwx_gtk2u-2.8.so.0.1.0 ../../wxfb/lib/libwx_gtk2u-2.8.so.0 
+
