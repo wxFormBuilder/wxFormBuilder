723d0570992247dad50e916e8ce8d8e91f3a4f99
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-03-15 19:45:26 +0000

    Moved 'premake' directory to the 'sdk' directory because this seems like a better more obvious location. You always need the sdk and so it makes sense that premake be added to the sdk. Added a 'misc' directory for other support files. It is small now but I think it will get more items as time goes on. Removed old files from the 'premake' directory. We are not going to use it anyways. :) Added a 'scripts' directory to the 'sdk/premake' to hold some of the upcoming helper Premake scripts. Added wxpresets.lua to the sdk/premake/scripts directory. This is a work in progress. It is not ready for heavy use. YET :-) Updated all readme's to point to the right location for build file generation.

--- 3.x/trunk/create_build_files.bat
+++ 3.x/trunk/create_build_files.bat
@@ -1,10 +1,10 @@
 @echo off
 
-premake\premake-win32.exe --target cb-gcc --unicode --with-wx-shared
+sdk\premake\premake-win32.exe --target cb-gcc --unicode --with-wx-shared
 echo done...
 echo.
 
-premake\premake-win32.exe --target gnu --unicode --with-wx-shared
+sdk\premake\premake-win32.exe --target gnu --unicode --with-wx-shared
 echo done...
 echo.
 
