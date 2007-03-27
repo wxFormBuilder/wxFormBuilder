0073b551bef45dbdba33b2ba23a8cdedb36b56d2
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-03-27 15:27:45 +0000

    added containers plugin

--- support/make_wxfb_archive.sh
+++ support/make_wxfb_archive.sh
@@ -6,6 +6,7 @@ rm ../wxfb/lib/*d.so
 cp bin/plugins/additional/libadditional.so ../wxfb/plugins/additional
 cp bin/plugins/common/libcommon.so ../wxfb/plugins/common
 cp bin/plugins/layout/liblayout.so ../wxfb/plugins/layout
+cp bin/plugins/containers/libcontainers.so ../wxfb/plugins/containers
 cp bin/plugins/wxAdditions/libwxadditions\-mini.so ../wxfb/plugins/wxAdditions
 cp /opt/wx/2.8.3/lib/libwx_gtk2u-2.8.so.0.1.1 ../wxfb/lib/libwx_gtk2u-2.8.so.0 
 
