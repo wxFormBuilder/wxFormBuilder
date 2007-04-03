560f6fdf9d301fa978c547fcdfcfb91b6bf6bf61
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-04-03 23:42:07 +0000

    Updated program icon. Added the initial try to get premake to generate a 'share' directory under Linux only.

--- 3.x/trunk/premake.lua
+++ 3.x/trunk/premake.lua
@@ -12,3 +12,31 @@ dopackage( "sdk/tinyxml" )
 dopackage( "src/controls/build/propgrid" )
 dopackage( "src/controls/build/wxFlatNotebook" )
 dopackage( "src/controls/build/wxScintilla" )
+
+-- Pre-build file creation steps
+function CreateShareReadme( filename )
+	local text = 
+	"This directory and the symlink it contains were created automatically by premake to\n" ..
+	"facilitate execution of wxFormBuilder prior to installation on Unix platforms. On Unix,\n" ..
+	"wxFormBuilder expects to be executed from a directory named \"output\", which is next to\n" ..
+	" a directory named \"share\". The \"share\" directory should have a subdirectory named\n" ..
+	"\"wxformbuilder\", which contains the configuration files. "
+	
+	if ( not os.fileexists( filename ) ) then
+		local fo = io.open( filename, "w" )
+		fo:write( text )
+		fo:close()
+	end
+end
+
+function CreateSymlink( symLink, pathToLink )
+	os.execute( "ln -s " .. pathToLink .. " "..symLink  )
+end
+
+if ( not windows ) then
+	print( "Running pre-build file creation steps..." )
+	-- Create a 'share' directory.
+	os.execute( "mkdir output/share" )
+	CreateShareReadme( "output/share/README.txt" )
+	CreateSymlink( "../output", "output/share/wxformbuilder" )
+end
