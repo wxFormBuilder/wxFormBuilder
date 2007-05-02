f0b2ee0ebc4c0fc34509a4542232703d188f55bd
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-05-02 13:21:23 +0000

    oops, missed these

---
+++ 3.x/trunk/src/rad/dataobject/dataobject.cpp
@@ -0,0 +1,125 @@
+#include "dataobject.h"
+#include "model/objectbase.h"
+#include "utils/typeconv.h"
+#include <ticpp.h>
+#include <string>
+#include "rad/appdata.h"
+#include <wx/utils.h>
+
+#include <ticpp.h>
+
+wxFBDataObject::wxFBDataObject( PObjectBase obj )
+:
+wxDataObject()
+{
+	if ( obj )
+	{
+		// create xml representation of ObjectBase
+		ticpp::Element element;
+		obj->SerializeObject( &element );
+
+		ticpp::Document doc;
+		doc.LinkEndChild( &element );
+		m_data = doc.GetAsString();
+	}
+}
+
+wxFBDataObject::~wxFBDataObject()
+{
+}
+
+void wxFBDataObject::GetAllFormats( wxDataFormat* formats, Direction dir ) const
+{
+	switch ( dir )
+	{
+		case Get:
+			formats[0] = wxFBDataObjectFormat;
+			formats[1] = wxDF_TEXT;
+			break;
+		case Set:
+			formats[0] = wxFBDataObjectFormat;
+			break;
+		default:
+			break;
+	}
+}
+
+bool wxFBDataObject::GetDataHere( const wxDataFormat&, void* buf ) const
+{
+	if ( NULL == buf )
+	{
+		return false;
+	}
+
+	memcpy( (char*)buf, m_data.c_str(), m_data.length() );
+
+	return true;
+}
+
+size_t wxFBDataObject::GetDataSize( const wxDataFormat& format ) const
+{
+	return m_data.length();
+}
+
+size_t wxFBDataObject::GetFormatCount( Direction dir ) const
+{
+	switch ( dir )
+	{
+		case Get:
+			return 2;
+		case Set:
+			return 1;
+		default:
+			return 0;
+	}
+}
+
+wxDataFormat wxFBDataObject::GetPreferredFormat( Direction dir ) const
+{
+	return wxFBDataObjectFormat;
+}
+
+bool wxFBDataObject::SetData( const wxDataFormat& format, size_t len, const void *buf )
+{
+	if ( format != wxFBDataObjectFormat )
+	{
+		return false;
+	}
+
+	m_data.assign( reinterpret_cast< const char* >( buf ), len );
+	return true;
+}
+
+PObjectBase wxFBDataObject::GetObj()
+{
+	if ( m_data.empty() )
+	{
+		return PObjectBase();
+	}
+
+	// Copy string to temporary FILE buffer
+	FILE* fileBuf = tmpfile();
+	int written = fwrite( m_data.c_str(), sizeof( char ), m_data.length(), fileBuf );
+	if ( (size_t)written < m_data.length() )
+	{
+		return PObjectBase();
+	}
+	rewind( fileBuf );
+
+	// Load FILE into TiXmlDocument
+	TiXmlDocument doc;
+	if ( !doc.LoadFile( fileBuf, TIXML_ENCODING_UTF8 ) )
+	{
+		return PObjectBase();
+	}
+
+	// Read ObjectBase from xml
+	TiXmlElement* element = doc.RootElement();
+	if ( NULL == element )
+	{
+		return PObjectBase();
+	}
+
+	PObjectDatabase db = AppData()->GetObjectDatabase();
+	return db->CreateObject( element );
+}
---
+++ 3.x/trunk/src/rad/dataobject/dataobject.h
@@ -0,0 +1,27 @@
+#ifndef __DATA_OBJECT__
+#define __DATA_OBJECT__
+
+#include <wx/dataobj.h>
+#include <wx/dnd.h>
+#include "utils/wxfbdefs.h"
+
+#define wxFBDataObjectFormat wxDataFormat( wxT("wxFormBuilderDataFormat") )
+
+class wxFBDataObject : public wxDataObject
+{
+private:
+	std::string m_data;
+
+public:
+	wxFBDataObject( PObjectBase obj = PObjectBase() );
+	~wxFBDataObject();
+	void GetAllFormats( wxDataFormat* formats, Direction dir = Get ) const;
+	bool GetDataHere( const wxDataFormat& format, void* buf ) const;
+	size_t GetDataSize(const wxDataFormat& format ) const;
+	size_t GetFormatCount( Direction dir = Get ) const;
+	wxDataFormat GetPreferredFormat( Direction dir = Get ) const;
+	bool SetData( const wxDataFormat& format, size_t len, const void *buf );
+	PObjectBase GetObj();
+};
+
+#endif //__DATA_OBJECT__
