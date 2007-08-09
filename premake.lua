f5010a36d92fffd39e581c69476ed264c69a8089
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-08-09 17:18:05 +0000

    Changed the default configuration when building to Release. Re-generate the build files to see this change.

--- premake.lua
+++ premake.lua
@@ -5,6 +5,10 @@ else
 	project.bindir = "output/lib"
 end
 
+-- Configureations. All I am doing is reordering them so that
+-- it defaults to a Release build.
+project.configs = { "Release", "Debug" }
+
 -- Add packages here.
 dopackage( "src" )
 dopackage( "plugins/additional" )
