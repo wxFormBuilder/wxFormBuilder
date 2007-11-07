69256df7eede1d577655e29432957424de7d6b33
auria <auria@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-11-07 01:22:29 +0000

    Added files to automatise distribution on OS X

---
+++ 3.x/trunk/install/macosx/makedist.sh
@@ -0,0 +1,9 @@
+dylibbundler -c ./install/macosx/dylibbundler.conf -l -x ./output/wxFormBuilder.app/Contents/MacOS/wxformbuilder -d ./output/wxFormBuilder.app/Contents/libs/ -cd -od
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libadditional.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libcommon.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libcontainers.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/liblayout.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib
+dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwxadditions-mini.dylib
