36785fa30f73767f10d63a957a95a55528bf3d3b
auria <auria@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-12-15 03:15:26 +0000

    Improved OS X application bundle construction

--- 3.x/trunk/install/macosx/postbuild.sh
+++ 3.x/trunk/install/macosx/postbuild.sh
@@ -1,49 +1,28 @@
 #!/bin/sh
 
-    export TOFILE=./output/wxFormBuilder.app/Contents/info.plist
+export TOFILE=./output/wxFormBuilder.app/Contents/info.plist
 
-	cd ..
+cd ..
 	
-	install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_flatnotebook-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib ./output/bin/wxformbuilder
-	install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_propgrid-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib ./output/bin/wxformbuilder
-	install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_scintilla-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib ./output/bin/wxformbuilder
-	install_name_tool -id @executable_path/../PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_flatnotebook-2.8_wxfb.dylib
-	install_name_tool -id @executable_path/../PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_propgrid-2.8_wxfb.dylib
-	install_name_tool -id @executable_path/../PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_scintilla-2.8_wxfb.dylib
+install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_flatnotebook-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib ./output/bin/wxformbuilder
+install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_propgrid-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib ./output/bin/wxformbuilder
+install_name_tool -change ../../../../output/lib/wxformbuilder/libwx_macu_scintilla-2.8_wxfb.dylib @executable_path/../PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib ./output/bin/wxformbuilder
+install_name_tool -id @executable_path/../PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_flatnotebook-2.8_wxfb.dylib
+install_name_tool -id @executable_path/../PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_propgrid-2.8_wxfb.dylib
+install_name_tool -id @executable_path/../PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib ./output/lib/wxformbuilder/libwx_macu_scintilla-2.8_wxfb.dylib
 
-	rm -r -f ./output/wxFormBuilder.app
-	mkdir ./output/wxFormBuilder.app	
-	mkdir ./output/wxFormBuilder.app/Contents
-	mkdir ./output/wxFormBuilder.app/Contents/Resources
-	mkdir ./output/wxFormBuilder.app/Contents/MacOS
-	mkdir ./output/wxFormBuilder.app/Contents/PlugIns
-	mkdir ./output/wxFormBuilder.app/Contents/SharedSupport
-	cp ./output/bin/wxformbuilder ./output/wxFormBuilder.app/Contents/MacOS/wxformbuilder
-	cp ./output/lib/wxformbuilder/* ./output/wxFormBuilder.app/Contents/PlugIns
-	cp -r ./output/plugins/ ./output/wxFormBuilder.app/Contents/SharedSupport/plugins
-	cp -r ./output/resources/ ./output/wxFormBuilder.app/Contents/SharedSupport/resources
-	cp -r ./output/xml/ ./output/wxFormBuilder.app/Contents/SharedSupport/xml
-	cp ./install/macosx/icon.icns ./output/wxFormBuilder.app/Contents/Resources/icon.icns
-	
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" >> $TOFILE
-	echo "<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">" >> $TOFILE
-	echo "<plist version=\"1.0\">" >> $TOFILE
-	echo "<dict>" >> $TOFILE
-	echo "	<key>CFBundleDevelopmentRegion</key>" >> $TOFILE
-	echo "	<string>English</string>" >> $TOFILE
-	echo "	<key>CFBundleExecutable</key>" >> $TOFILE
-	echo "	<string>wxformbuilder</string>" >> $TOFILE
-	echo "	<key>CFBundleInfoDictionaryVersion</key>" >> $TOFILE
-	echo "	<string>6.0</string>" >> $TOFILE
-	echo "	<key>CFBundleName</key>" >> $TOFILE
-	echo "	<string>wxFormBuilder</string>" >> $TOFILE
-	echo "	<key>CFBundleIconFile</key>" >> $TOFILE
-	echo "	<string>icon.icns</string>" >> $TOFILE
-	echo "	<key>CFBundlePackageType</key>" >> $TOFILE
-	echo "	<string>APPL</string>" >> $TOFILE
-	echo "	<key>CFBundleVersion</key>" >> $TOFILE
-	echo "	<string>3.0</string>" >> $TOFILE
-	echo "	<key>CSResourcesFileMapped</key>" >> $TOFILE
-	echo "	<true/>	" >> $TOFILE
-	echo "</dict>" >> $TOFILE
-	echo "</plist>" >> $TOFILE
+rm -r -f ./output/wxFormBuilder.app
+mkdir ./output/wxFormBuilder.app	
+mkdir ./output/wxFormBuilder.app/Contents
+mkdir ./output/wxFormBuilder.app/Contents/Resources
+mkdir ./output/wxFormBuilder.app/Contents/MacOS
+mkdir ./output/wxFormBuilder.app/Contents/PlugIns
+mkdir ./output/wxFormBuilder.app/Contents/SharedSupport
+cp ./output/bin/wxformbuilder ./output/wxFormBuilder.app/Contents/MacOS/wxformbuilder
+cp ./output/lib/wxformbuilder/* ./output/wxFormBuilder.app/Contents/PlugIns
+cp -r ./output/plugins/ ./output/wxFormBuilder.app/Contents/SharedSupport/plugins
+cp -r ./output/resources/ ./output/wxFormBuilder.app/Contents/SharedSupport/resources
+cp -r ./output/xml/ ./output/wxFormBuilder.app/Contents/SharedSupport/xml
+cp ./install/macosx/icon.icns ./output/wxFormBuilder.app/Contents/Resources/icon.icns
+cp ./install/macosx/docicon.icns ./output/wxFormBuilder.app/Contents/Resources/docicon.icns
+cp ./install/macosx/Info.plist ./output/wxFormBuilder.app/Contents/Info.plist
