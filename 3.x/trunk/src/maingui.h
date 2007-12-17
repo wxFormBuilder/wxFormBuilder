a23f0988977bb768300157d60f5c71b621fe49be
auria <auria@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2007-12-17 19:46:52 +0000

    Perform file associations on mac

--- 3.x/trunk/src/maingui.h
+++ 3.x/trunk/src/maingui.h
@@ -3,12 +3,16 @@
 
 #include <wx/app.h>
 
+class MainFrame;
+
 class MyApp : public wxApp
 {
 private:
   wxLog * m_old_log;
   wxLogWindow * m_log;
 
+  MainFrame *m_frame;
+  
 public:
   bool OnInit();
   #if wxUSE_ON_FATAL_EXCEPTION && wxUSE_STACKWALKER
@@ -17,6 +21,11 @@ public:
   int OnRun();
   int OnExit();
   ~MyApp();
+  
+  #ifdef __WXMAC__
+  wxString m_mac_file_name;
+  void MacOpenFile(const wxString &fileName);
+  #endif
 };
 
 DECLARE_APP(MyApp)
