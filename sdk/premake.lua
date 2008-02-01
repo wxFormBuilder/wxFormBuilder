0a1bb78b6c6f455386a29d264d5359bd635a7974
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-02-01 16:08:23 +0000

    Added support for dynamic runtime.

--- sdk/premake.lua
+++ sdk/premake.lua
@@ -3,3 +3,8 @@ project.name = "wxFormBuilder SDK"
 -- Add sdk projects here. (This needs to match the directory name not the package name.)
 dopackage( "plugin_interface" )
 dopackage( "tinyxml" )
+
+-- Add options here.
+addoption( "dynamic-runtime", "Use the dynamicly loadable version of the runtime." )
+addoption( "unicode", "Use the Unicode character set" )
+
