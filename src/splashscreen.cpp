5e81ac175a6b31a8ce8997a5784b120eeb736933
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-02-10 18:55:00 +0000

    GPL'd splashscreen, with the approval of Ceniza

--- src/splashscreen.cpp
+++ src/splashscreen.cpp
@@ -1,11 +1,26 @@
 /* cbSplashScreen.cpp
  *
  * READ THE HEADER FILE COMMENT FOR A DESCRIPTION.
- *   It was coded to be used in Code::Blocks IDE but I don't care if you use it for your own projects.
  *
  * Author: Ceniza
+ *
+ * Copyright (C) 2006 Ceniza
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
+ 
 #include "splashscreen.h"
 #include <wx/dcclient.h>
 #include <wx/dcmemory.h>
