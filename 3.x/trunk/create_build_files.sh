60cdc3c7c1f9e51a33632bd2f1fe4a489ec8d69c
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-02-17 02:59:33 +0000

    now externalling premake 3.5 and building it with create_build_files.sh this will make the build architecture independent, should also work for mac should only need to build premake on the first call to create_build_files.sh

--- 3.x/trunk/create_build_files.sh
+++ 3.x/trunk/create_build_files.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #
 
+set -e
+
+# Build premake
+make -Csdk/premake/src -f../build/Makefile
+
 # Autodetect wxWidgets settings
 if wx-config --unicode >/dev/null 2>/dev/null; then
 	unicode="--unicode"
@@ -9,11 +14,11 @@ if ! wx-config --debug >/dev/null 2>/dev/null; then
 	debug="--disable-wx-debug"
 fi
 
-sdk/premake/premake-linux --target cb-gcc $unicode $debug --with-wx-shared $1
+sdk/premake/src/bin/premake --target cb-gcc $unicode $debug --with-wx-shared $1
 echo done...
 echo 
 #
-sdk/premake/premake-linux --target gnu $unicode $debug --with-wx-shared $1
+sdk/premake/src/bin/premake --target gnu $unicode $debug --with-wx-shared $1
 echo done...
 echo 
 #
