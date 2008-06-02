3241fd3a8c0bb23ff8f9f8372e72ba50fb8a67ca
auria <auria@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2008-06-02 14:45:33 +0000

    updated mac OS X packaging files

--- 3.x/trunk/install/macosx/makedist.sh
+++ 3.x/trunk/install/macosx/makedist.sh
@@ -21,15 +21,18 @@
 # uses tool macdylibbundler.sf.net to integrate wxWidgets libraries
 # inside the app bundle. Not necessary if you require the user to install
 # them on his side, or if you use the ones that come with the system on Leopard
-#
-# will also be different if you built monolithic
 
-dylibbundler -c ./install/macosx/dylibbundler.conf -l -x ./output/wxFormBuilder.app/Contents/MacOS/wxformbuilder -d ./output/wxFormBuilder.app/Contents/libs/ -cd -od
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libadditional.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libcommon.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libcontainers.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/liblayout.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_flatnotebook-2.8_wxfb.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_propgrid-2.8_wxfb.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwx_macu_scintilla-2.8_wxfb.dylib
-dylibbundler -c ./install/macosx/dylibbundler.conf -x ./output/wxFormBuilder.app/Contents/PlugIns/libwxadditions-mini.dylib
+PLUG_IN_PATH="./output/wxFormBuilder.app/Contents/PlugIns"
+
+FILES="-x ./output/wxFormBuilder.app/Contents/MacOS/wxformbuilder \
+-x $PLUG_IN_PATH/libadditional.dylib \
+-x $PLUG_IN_PATH/libcommon.dylib \
+-x $PLUG_IN_PATH/libcontainers.dylib \
+-x $PLUG_IN_PATH/liblayout.dylib \
+-x $PLUG_IN_PATH/libwx_macu_flatnotebook-2.8_wxfb.dylib \
+-x $PLUG_IN_PATH/libwx_macu_propgrid-2.8_wxfb.dylib \
+-x $PLUG_IN_PATH/libwx_macu_scintilla-2.8_wxfb.dylib \
+-x $PLUG_IN_PATH/libwxadditions-mini.dylib"
+
+
+dylibbundler -od -b -d ./output/wxFormBuilder.app/Contents/libs/ $FILES
