02d5f36412c6811c23d13ec18f707821fe5449c4
rjmyst3 <rjmyst3@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2009-01-26 16:07:13 +0000

    switch from CodeBlocks to CodeLite (codelite.org)

--- 3.x/trunk/create_build_files.bat
+++ 3.x/trunk/create_build_files.bat
@@ -1,6 +1,6 @@
 @echo off
 
-sdk\premake\premake-win32.exe --target cb-gcc --unicode --with-wx-shared
+sdk\premake\premake-win32.exe --target cl-gcc --unicode --with-wx-shared
 echo done...
 echo.
 
