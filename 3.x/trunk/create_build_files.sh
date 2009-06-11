99247cdde0e73b4c8b0e5263793a0dd5d2a7f657
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2009-06-11 13:39:46 +0000

    Made sure the line endings are consistant.

--- 3.x/trunk/create_build_files.sh
+++ 3.x/trunk/create_build_files.sh
@@ -5,7 +5,7 @@ set -e
 
 # Build premake
 PREMAKE_DIR=sdk/premake
-make CONFIG=Release -C$PREMAKE_DIR/src -f../build/Makefile 
+make CONFIG=Release -C$PREMAKE_DIR/src -f../build/Makefile
 
 # Autodetect wxWidgets settings
 if wx-config --unicode >/dev/null 2>/dev/null; then
@@ -17,11 +17,11 @@ fi
 
 $PREMAKE_DIR/bin/premake --target cb-gcc $unicode $debug --with-wx-shared $1
 echo done...
-echo 
+echo
 #
 $PREMAKE_DIR/bin/premake --target gnu $unicode $debug --with-wx-shared $1
 echo done...
-echo 
+echo
 #
 echo Done generating all project files for wxFormBuilder.
 #
