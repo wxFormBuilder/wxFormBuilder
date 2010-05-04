e695104c1cdca8205ac8a9a6d0518484f9bbcfa5
rjpcomputing <rjpcomputing@1bf5dfe7-1b0e-0410-9632-deacb7090205>
2010-05-04 18:22:24 +0000

    updated debian packaging scripts to automate most of it. Upreved for release.

--- 3.x/trunk/install/linux/dsc.sh
+++ 3.x/trunk/install/linux/dsc.sh
@@ -25,26 +25,84 @@ then
    exit 1
 fi
 
+function pause()
+{
+   read -p “$*”
+}
+
+# this function does the actual work of making the source package
+# the version is passed to it as the first argument, below
+function add_log_entry
+{
+	set -e
+
+	# check for commandlike arguments
+	if [ -z "$2" ]
+	then
+		echo "User name missing"
+		exit 1
+	fi
+
+	if [ -z "$3" ]
+	then
+		echo "Email missing"
+		exit 1
+	fi
+
+	# use debchange
+	echo debchange -D lucid -v "$1-0ubuntu1~lucid1" -m
+	debchange -D lucid -v "$1-0ubuntu1~lucid1" -m
+}
+
+
 # get source tarball
 debian/rules get-orig-source
 
 #extract it
 tar xzf wxformbuilder_*.orig.tar.gz
 
-#determine version
-changelog=`cd wxformbuilder_*.orig/install/linux && dpkg-parsechangelog`
-version=`expr match "$changelog" '.*Version: \([0-9]\.[0-9]\{1,2\}\.[0-9]\+\).*'`
-
-#rename it
-sourcedir=wxformbuilder-$version
-mv wxformbuilder_*.orig $sourcedir
-cp -R $sourcedir/install/linux/debian $sourcedir/debian
-
 #save current dir
 currentdir=`pwd`
 
-#enter source dir
-cd $sourcedir
+#save the source dir
+cd wxformbuilder-*
+sourcedir=`pwd`
+
+#determine version
+#changelog="output/Changelog.txt"
+#if [ ! -f $changelog ];
+#then
+#  echo "Sorry, could not find "$changelog". Need it to parse the version."
+#  exit 1
+#fi
+#
+#cat "$changelog" |
+#while read line;
+#do
+# version=`expr match "$line" '.*\([0-9]\.[0-9]\{1,2\}\.[0-9]\+\)'`
+# if [ -n "$version" ]
+# then
+# echo "$version"
+#   # because I redirected cat to the while loop, bash spawned a subshell
+#   # this means "version" will go out of scope at the end of the loop
+#   # so I need to do everything here
+#   if [ ${#version} -ge 7 ]
+#   then
+#     version=${version/0/}
+#   fi
+#   add_log_entry $version $1 $2
+#   break
+# fi
+#done
+
+#copy the debian directory to the correct directory
+cp -R install/linux/debian .
+
+nano debian/changelog
+
+#update the changelog by hand for now
+echo "Press any key to continue…"
+pause
 
 #make debian source archive
 dpkg-buildpackage -S -sa -rfakeroot
@@ -52,3 +110,6 @@ dpkg-buildpackage -S -sa -rfakeroot
 #cleanup
 cd $currentdir
 rm -r $sourcedir
+
+#upload the files to the PPA
+dput ppa:wxformbuilder/release *.changes
