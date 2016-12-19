f0d72c2027798eda14df8f0bdb6065df348d5241
Tobias Taschner <TcT2k@users.noreply.github.com>
2016-12-19 13:41:03 +0100

    Build as application bundle for macOS
    Create a application bundle containing all resources and libraries.
    Fixup the bundle when installing to package all required libraries.

--- CMakeModules/add_wxformbuilder_plugin_target.cmake
+++ CMakeModules/add_wxformbuilder_plugin_target.cmake
@@ -9,25 +9,48 @@ macro(add_wxformbuilder_plugin_target plugin_name)
     target_link_libraries(${plugin_name} wxfb-plugin-interface)
     target_compile_definitions(${plugin_name} PRIVATE BUILD_DLL)
 
-    add_custom_command(TARGET ${plugin_name} POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/icons 
-        $<TARGET_PROPERTY:wxFormBuilder,BINARY_DIR>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/icons
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/xml 
-        $<TARGET_PROPERTY:wxFormBuilder,BINARY_DIR>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/xml
-    )
+    if(APPLE)
+        add_custom_command(TARGET ${plugin_name} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/icons 
+            $<TARGET_FILE_DIR:wxFormBuilder>/../${INSTALL_SHARE_DIR}/plugins/${plugin_name}/icons
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/xml 
+            $<TARGET_FILE_DIR:wxFormBuilder>/../${INSTALL_SHARE_DIR}/plugins/${plugin_name}/xml
+        )
+    elseif(WIN32)
+        add_custom_command(TARGET ${plugin_name} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/icons 
+            $<TARGET_PROPERTY:wxFormBuilder,BINARY_DIR>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/icons
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/xml 
+            $<TARGET_PROPERTY:wxFormBuilder,BINARY_DIR>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/xml
+        )
+    else()
+        add_custom_command(TARGET ${plugin_name} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/icons 
+            $<TARGET_FILE_DIR:wxFormBuilder>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/icons
+            COMMAND ${CMAKE_COMMAND} -E copy_directory ${CMAKE_CURRENT_SOURCE_DIR}/xml 
+            $<TARGET_FILE_DIR:wxFormBuilder>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}/xml
+        )
+    endif()
 
     if(MSVC)
         set_target_properties(${plugin_name} PROPERTIES
             OUTPUT_NAME lib${plugin_name}
             RUNTIME_OUTPUT_DIRECTORY $<TARGET_PROPERTY:wxFormBuilder,BINARY_DIR>/${INSTALL_SHARE_DIR}/plugins/${plugin_name}$<0:_>
             )
+    elseif(APPLE)
+        set_target_properties(${plugin_name} PROPERTIES
+            ARCHIVE_OUTPUT_DIRECTORY $<TARGET_FILE_DIR:wxFormBuilder>/../PlugIns
+            LIBRARY_OUTPUT_DIRECTORY $<TARGET_FILE_DIR:wxFormBuilder>/../PlugIns
+            )
     endif()
 
-    if(WIN32)
-        set(plugin_target_dir ${INSTALL_SHARE_DIR}/plugins/${plugin_name})
-    else()
-        set(plugin_target_dir lib/wxformbuilder)
+    if(NOT APPLE)
+        if(WIN32)
+            set(plugin_target_dir ${INSTALL_SHARE_DIR}/plugins/${plugin_name})
+        else()
+            set(plugin_target_dir lib/wxformbuilder)
+        endif()
+        install(TARGETS ${plugin_name} LIBRARY DESTINATION ${plugin_target_dir} RUNTIME DESTINATION ${plugin_target_dir})
+        install(DIRECTORY icons xml DESTINATION ${INSTALL_SHARE_DIR}/plugins/${plugin_name} OPTIONAL)
     endif()
-    install(TARGETS ${plugin_name} LIBRARY DESTINATION lib/wxformbuilder RUNTIME DESTINATION ${plugin_target_dir})
-    install(DIRECTORY icons xml DESTINATION ${INSTALL_SHARE_DIR}/plugins/${plugin_name} OPTIONAL)
 endmacro(add_wxformbuilder_plugin_target)
