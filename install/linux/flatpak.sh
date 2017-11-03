9e420f39ee2ae2577fc2a8ec4738617a67446a80
Jan Niklas Hasse <jhasse@bixense.com>
2017-11-03 12:01:33 +0100

    Flatpak support for Linux, fix #273

---
+++ install/linux/flatpak.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+dnf install -y flatpak-builder
+flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
+flatpak install gnome org.freedesktop.Platform 1.6
+flatpak install gnome org.freedesktop.Sdk 1.6
+flatpak-builder --repo=repo ./build org.wxformbuilder.wxFormBuilder.json
+flatpak build-bundle repo wxformbuilder.flatpak org.wxformbuilder.wxFormBuilder
