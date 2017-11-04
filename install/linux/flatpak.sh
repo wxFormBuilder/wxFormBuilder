bda1e7fd0f3333c26db312967f5126999b447619
Jan Niklas Hasse <jhasse@bixense.com>
2017-11-04 14:17:41 +0100

    Travis CI: Disable caching again; Upgrade Boost on macOS

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -5,7 +5,5 @@ dnf install -y flatpak-builder git
 flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
 flatpak install gnome org.freedesktop.Platform 1.6
 flatpak install gnome org.freedesktop.Sdk 1.6
-
-export FLATPAK_OSTREE_REPO_MODE=user-only # See https://github.com/flatpak/flatpak-builder/issues/57
 flatpak-builder --repo=repo ./build org.wxformbuilder.wxFormBuilder.json
 flatpak build-bundle repo wxformbuilder.flatpak org.wxformbuilder.wxFormBuilder
