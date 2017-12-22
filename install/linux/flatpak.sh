b08d2c67c62e08ff8f28ceb9a26f6e9a24100223
Jan Niklas Hasse <jhasse@bixense.com>
2017-12-22 14:56:50 +0100

    Travis CI: flatpak-builder should be up-to-date now hopefully

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -3,8 +3,6 @@ set -eux
 
 rpm --import https://getfedora.org/static/9DB62FB1.txt
 dnf install -y flatpak-builder git wget
-wget https://kojipkgs.fedoraproject.org//packages/flatpak-builder/0.10.4/1.fc28/x86_64/flatpak-builder-0.10.4-1.fc28.x86_64.rpm
-dnf install -y ./flatpak-builder*.rpm
 flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
 flatpak install gnome org.freedesktop.Platform 1.6
 flatpak install gnome org.freedesktop.Sdk 1.6
