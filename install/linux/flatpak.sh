17cd5087521c8c897060cc7bd3965515f28cfdcb
Jan Niklas Hasse <jhasse@bixense.com>
2018-01-22 16:11:28 +0100

    Travis CI: Fix Linux build error by updating first

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -2,6 +2,7 @@
 set -eux
 
 rpm --import https://getfedora.org/static/9DB62FB1.txt
+dnf upgrade -y
 dnf install -y flatpak-builder git wget
 flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
 flatpak install gnome org.freedesktop.Platform 1.6
