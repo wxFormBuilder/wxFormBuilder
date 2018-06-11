614965a84aa1f422f0d7cdfa29e906e3bb2e60ce
Jan Niklas Hasse <jhasse@bixense.com>
2018-06-11 11:38:32 +0200

    Travis CI: Use Fedora 28 instead of Rawhide

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
 
-rpm --import https://getfedora.org/static/9DB62FB1.txt
 dnf upgrade -y
 dnf install -y flatpak-builder git wget
 flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
