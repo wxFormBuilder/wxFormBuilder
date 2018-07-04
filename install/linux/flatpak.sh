0915a2f712e762edd5d8466c6ac28379b0cefcc0
Jan Niklas Hasse <jhasse@bixense.com>
2018-07-04 08:56:22 +0200

    Add -y flag for `flatpak install`

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -4,7 +4,7 @@ set -eux
 dnf upgrade -y
 dnf install -y flatpak-builder git wget
 flatpak remote-add --from gnome https://sdk.gnome.org/gnome.flatpakrepo
-flatpak install gnome org.freedesktop.Platform 1.6
-flatpak install gnome org.freedesktop.Sdk 1.6
+flatpak install -y gnome org.freedesktop.Platform 1.6
+flatpak install -y gnome org.freedesktop.Sdk 1.6
 flatpak-builder --repo=repo ./build org.wxformbuilder.wxFormBuilder.json
 flatpak build-bundle repo wxformbuilder.flatpak org.wxformbuilder.wxFormBuilder
