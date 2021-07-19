bf8141f8b634f55257ee0a291d2d72cb590dba98
jolz <jolz@freenet.de>
2021-07-19 07:53:27 +0200

    update flatpak platform to 20.08
    keep dnf-calls für travis-CI as @sodevel requested.
    The above *should* word, but untested. sorry. 
    
    I was no aware of a Travis-CI-Flow - sorry I'm just learning writing "Pull requests".

--- install/linux/flatpak.sh
+++ install/linux/flatpak.sh
@@ -3,14 +3,15 @@ set -eux
 
 FLATHUB=https://flathub.org/repo/flathub.flatpakrepo
 
+# needed in github-ci
+command -v dnf && { dnf upgrade -y ; dnf install -y flatpak-builder git wget; }
+
 NOTFOUND=""
 command -v flatpak         >/dev/null 2>&1 || NOTFOUND="$NOTFOUND flatpak"
 command -v flatpak-builder >/dev/null 2>&1 || NOTFOUND="$NOTFOUND flatpak-builder"
 command -v git             >/dev/null 2>&1 || NOTFOUND="$NOTFOUND git"
 command -v wget            >/dev/null 2>&1 || NOTFOUND="$NOTFOUND wget"
 test -z "$NOTFOUND" || { echo "$NOTFOUND: not found, but needed. please install. Aborting. "; exit 1; }
-#dnf upgrade -y
-#dnf install -y flatpak-builder git wget
 
 flatpak --user remote-add --if-not-exists flathub $FLATHUB
 flatpak --user install -y flathub org.freedesktop.Platform 20.08
