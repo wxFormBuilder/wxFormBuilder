8488ac485d76831ea60834ac49e988d4b9f963a1
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-09-10 18:58:06 +0200

    Remove variable quoting where not strictly necessary
    Require plugin names and directory to not contain spaces.

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -5,56 +5,56 @@ function(add_plugin PLUGIN_NAME)
   cmake_parse_arguments(PLUGIN "${options}" "${singleValues}" "${multiValues}" ${ARGN})
 
   if (NOT DEFINED PLUGIN_DIRECTORY)
-    set(PLUGIN_DIRECTORY "${PLUGIN_NAME}")
+    set(PLUGIN_DIRECTORY ${PLUGIN_NAME})
   endif()
   if(DEFINED PLUGIN_SOURCES)
-    list(TRANSFORM PLUGIN_SOURCES PREPEND "${PLUGIN_DIRECTORY}/")
+    list(TRANSFORM PLUGIN_SOURCES PREPEND ${PLUGIN_DIRECTORY}/)
   endif()
 
-  add_library("wxFormBuilder_${PLUGIN_NAME}" MODULE)
-  add_library("wxFormBuilder::${PLUGIN_NAME}" ALIAS "wxFormBuilder_${PLUGIN_NAME}")
-  set_target_properties("wxFormBuilder_${PLUGIN_NAME}" PROPERTIES
-      OUTPUT_NAME "$<$<BOOL:${WIN32}>:lib>${PLUGIN_NAME}"
+  add_library(wxFormBuilder_${PLUGIN_NAME} MODULE)
+  add_library(wxFormBuilder::${PLUGIN_NAME} ALIAS wxFormBuilder_${PLUGIN_NAME})
+  set_target_properties(wxFormBuilder_${PLUGIN_NAME} PROPERTIES
+      OUTPUT_NAME $<$<BOOL:${WIN32}>:lib>${PLUGIN_NAME}
   )
 
-  target_sources("wxFormBuilder_${PLUGIN_NAME}"
+  target_sources(wxFormBuilder_${PLUGIN_NAME}
     PRIVATE
-      "${PLUGIN_DIRECTORY}/${PLUGIN_NAME}.cpp"
+      ${PLUGIN_DIRECTORY}/${PLUGIN_NAME}.cpp
   )
   if(DEFINED PLUGIN_SOURCES)
-    target_sources("wxFormBuilder_${PLUGIN_NAME}"
+    target_sources(wxFormBuilder_${PLUGIN_NAME}
       PRIVATE
         ${PLUGIN_SOURCES}
     )
   endif()
   if(DEFINED PLUGIN_DEFINITIONS)
-    target_compile_definitions("wxFormBuilder_${PLUGIN_NAME}"
+    target_compile_definitions(wxFormBuilder_${PLUGIN_NAME}
       PRIVATE
         ${PLUGIN_DEFINITIONS}
     )
   endif()
-  target_link_libraries("wxFormBuilder_${PLUGIN_NAME}"
+  target_link_libraries(wxFormBuilder_${PLUGIN_NAME}
     PRIVATE
       wxFormBuilder::plugin-interface
   )
   if(DEFINED PLUGIN_LIBRARIES)
-    target_link_libraries("wxFormBuilder_${PLUGIN_NAME}"
+    target_link_libraries(wxFormBuilder_${PLUGIN_NAME}
       PRIVATE
         ${PLUGIN_LIBRARIES}
     )
   endif()
 
-  install(TARGETS "wxFormBuilder_${PLUGIN_NAME}"
+  install(TARGETS wxFormBuilder_${PLUGIN_NAME}
     RUNTIME
-      DESTINATION "${CMAKE_INSTALL_BINDIR}/wxformbuilder"
+      DESTINATION ${CMAKE_INSTALL_BINDIR}/wxformbuilder
     LIBRARY
-      DESTINATION "${CMAKE_INSTALL_LIBDIR}/wxformbuilder"
+      DESTINATION ${CMAKE_INSTALL_LIBDIR}/wxformbuilder
   )
   # TODO: After changing the layout, maybe use an own CMakeLists for resources and even own targets for them
-  install(DIRECTORY "../output/plugins/${PLUGIN_DIRECTORY}/icons"
-    DESTINATION "${CMAKE_INSTALL_DATADIR}/wxformbuilder/plugins/${PLUGIN_NAME}"
+  install(DIRECTORY ../output/plugins/${PLUGIN_DIRECTORY}/icons
+    DESTINATION ${CMAKE_INSTALL_DATADIR}/wxformbuilder/plugins/${PLUGIN_NAME}
   )
-  install(DIRECTORY "../output/plugins/${PLUGIN_DIRECTORY}/xml"
-    DESTINATION "${CMAKE_INSTALL_DATADIR}/wxformbuilder/plugins/${PLUGIN_NAME}"
+  install(DIRECTORY ../output/plugins/${PLUGIN_DIRECTORY}/xml
+    DESTINATION ${CMAKE_INSTALL_DATADIR}/wxformbuilder/plugins/${PLUGIN_NAME}
   )
 endfunction()
