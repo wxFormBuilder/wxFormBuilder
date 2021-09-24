d746c06589f7b37423618ee26a3f3b1a4d1efa7e
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-09-25 00:16:22 +0200

    Fix building with PreMake and Meson
    These systems don't generate the required source files, add a second
    code path that returns hardcoded values.

--- src/rad/revision.h
+++ src/rad/revision.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#ifdef WXFB_VERSION_GENERATE
 const char* getRevision();
 
 const char* getStrippedRevision(const char* version);
 
 const char* getReleaseRevision(const char* version);
+#else
+inline const char* getRevision() { return ""; }
+
+inline const char* getStrippedRevision(const char* version) { return ""; }
+
+inline const char* getReleaseRevision(const char* version) { return ""; }
+#endif
--- src/rad/version.h
+++ src/rad/version.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#ifdef WXFB_VERSION_GENERATE
 const char* getVersion();
+#else
+inline const char* getVersion() { return "3.9.0"; }
+#endif
