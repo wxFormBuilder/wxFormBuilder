9e9e2bf4805a46d14cb72180b119aedd9e741ffe
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-10-01 00:14:17 +0200

    Determine more git details
    Breaking change: GIT_REVISION now always contains the short revision hash
    
    The git describe output is now stored in GIT_DESCRIBE, if available.
    The closest tag, its distance and the dirty state are determined by
    individual commands, this should be more safe than parsing the
    describe output.

--- cmake/gitrevision.cmake
+++ cmake/gitrevision.cmake
@@ -12,47 +12,80 @@ Input parameters:
 - gitOutput: Path to the generated file
 
 Template parameters:
-- GIT_REVISION: Revision description
+- GIT_REVISION: Revision short hash
+- GIT_DESCRIBE: Long describe output
+- GIT_TAG_NAME: Name of closest tag
+- GIT_TAG_DISTANCE: Distance in commits to closest tag
+- GIT_DIRTY: Dirty flag of working copy
 ]]
 
 set(GIT_REVISION "")
+set(GIT_DESCRIBE "")
+set(GIT_TAG_NAME "")
+set(GIT_TAG_DISTANCE "0")
+set(GIT_DIRTY "false")
+
+if(gitRequired)
+  set(git_message_level WARNING)
+else()
+  set(git_message_level FATAL_ERROR)
+endif()
 
 find_program(git_cmd NAMES git)
 if (git_cmd)
   execute_process(
-    COMMAND "${git_cmd}" describe --long --dirty
+    COMMAND "${git_cmd}" rev-parse --short HEAD
     WORKING_DIRECTORY "${gitDirectory}"
     RESULT_VARIABLE git_result
     OUTPUT_VARIABLE GIT_REVISION
     OUTPUT_STRIP_TRAILING_WHITESPACE
   )
-  # A non-zero return code means error
-  if(git_result)
-    message(WARNING "Git could not determine a revision by describe, trying rev-parse")
+  if(NOT git_result)
     execute_process(
-      COMMAND "${git_cmd}" rev-parse --short HEAD
+      COMMAND "${git_cmd}" describe --long --dirty
       WORKING_DIRECTORY "${gitDirectory}"
       RESULT_VARIABLE git_result
-      OUTPUT_VARIABLE GIT_REVISION
+      OUTPUT_VARIABLE GIT_DESCRIBE
       OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-    if(git_result)
-      if(gitRequired)
-        message(FATAL_ERROR "Git could not determine a revision by rev-parse")
-      else()
-        message(WARNING "Git could not determine a revision by rev-parse, using empty revision")
-      endif()
+    if(NOT git_result)
+      execute_process(
+        COMMAND "${git_cmd}" describe --abbrev=0
+        WORKING_DIRECTORY "${gitDirectory}"
+        RESULT_VARIABLE git_result
+        OUTPUT_VARIABLE GIT_TAG_NAME
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+      execute_process(
+        COMMAND "${git_cmd}" rev-list --count "${GIT_TAG_NAME}..HEAD"
+        WORKING_DIRECTORY "${gitDirectory}"
+        RESULT_VARIABLE git_result
+        OUTPUT_VARIABLE GIT_TAG_DISTANCE
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
     else()
-      # TODO: A hack for getStrippedRevision(const char*) to produce a leading dash
-      string(PREPEND GIT_REVISION "-")
+      # Currently describe is optional
+      message(WARNING "Git could not describe")
     endif()
+    execute_process(
+      COMMAND "${git_cmd}" status --porcelain --ignore-submodules=none -unormal
+      WORKING_DIRECTORY "${gitDirectory}"
+      RESULT_VARIABLE git_result
+      OUTPUT_VARIABLE git_discard
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+      ERROR_VARIABLE git_discard
+      ERROR_STRIP_TRAILING_WHITESPACE
+    )
+    if(NOT git_result AND NOT git_discard)
+      set(GIT_DIRTY "false")
+    else()
+      set(GIT_DIRTY "true")
+    endif()
+  else()
+    message(${git_message_level} "Git could not determine a revision")
   endif()
 else()
-  if(gitRequired)
-    message(FATAL_ERROR "Git not found")
-  else()
-    message(WARNING "Git not found, using empty revision")
-  endif()
+  message(${git_message_level} "Git not found")
 endif()
 
 configure_file("${gitTemplate}" "${gitOutput}" @ONLY)
