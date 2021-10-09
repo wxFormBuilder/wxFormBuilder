d4db6f227ead9ec63ffcd6d979a88c49a36df2bc
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-10-09 03:25:23 +0200

    Add options to specify plugin components and icons
    With these options it is possible (and required) to specify plugin
    components and icons. This will validate if the required files
    (definitions and code templates) are present and process only
    the given files.

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -1,14 +1,21 @@
 function(add_plugin PLUGIN_NAME)
   set(options "")
   set(singleValues DIRECTORY)
-  set(multiValues SOURCES DEFINITIONS LIBRARIES)
+  set(multiValues SOURCES DEFINITIONS LIBRARIES COMPONENTS ICONS)
   cmake_parse_arguments(PLUGIN "${options}" "${singleValues}" "${multiValues}" ${ARGN})
 
-  if (NOT DEFINED PLUGIN_DIRECTORY)
+  if(NOT DEFINED PLUGIN_DIRECTORY)
     set(PLUGIN_DIRECTORY ${PLUGIN_NAME})
   endif()
   if(DEFINED PLUGIN_SOURCES)
     list(TRANSFORM PLUGIN_SOURCES PREPEND ${PLUGIN_DIRECTORY}/)
+  endif()
+  if(NOT DEFINED PLUGIN_COMPONENTS)
+    set(PLUGIN_COMPONENTS ${PLUGIN_NAME})
+  endif()
+  if(DEFINED PLUGIN_ICONS)
+    # TODO: In the final sources layout these files should reside next to the sources
+    list(TRANSFORM PLUGIN_ICONS PREPEND ../output/plugins/${PLUGIN_DIRECTORY}/icons/)
   endif()
 
   add_library(wxFormBuilder_${PLUGIN_NAME} MODULE)
@@ -54,6 +61,18 @@ function(add_plugin PLUGIN_NAME)
     )
   endif()
 
+  set(generatorLanguages cpp python lua php)
+  set(PLUGIN_COMPONENTS_DEFINITIONS "")
+  set(PLUGIN_COMPONENTS_TEMPLATES "")
+  foreach(component IN LISTS PLUGIN_COMPONENTS)
+    # TODO: In the final sources layout these files should reside next to the sources  
+    list(APPEND PLUGIN_COMPONENTS_DEFINITIONS ../output/plugins/${PLUGIN_DIRECTORY}/xml/${component}.xml)
+    foreach(lang IN LISTS generatorLanguages)
+      # TODO: In the final sources layout these files should reside next to the sources
+      list(APPEND PLUGIN_COMPONENTS_TEMPLATES ../output/plugins/${PLUGIN_DIRECTORY}/xml/${component}.${lang}code)
+    endforeach()
+  endforeach()
+
   if(WIN32)
     # TODO: Using RUNTIME_DEPENDENCIES results in the dependencies getting installed in the locations
     #       defined in this command, this is however unwanted because they should get installed in
@@ -81,11 +100,17 @@ function(add_plugin PLUGIN_NAME)
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/wxformbuilder
     )
   endif()
-  # TODO: After changing the layout, maybe use an own CMakeLists for resources and even own targets for them
-  install(DIRECTORY ../output/plugins/${PLUGIN_DIRECTORY}/icons
-    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}
+
+  install(FILES ${PLUGIN_COMPONENTS_DEFINITIONS}
+    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/xml
   )
-  install(DIRECTORY ../output/plugins/${PLUGIN_DIRECTORY}/xml
-    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}
+  install(FILES ${PLUGIN_COMPONENTS_TEMPLATES}
+    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/xml
   )
+  if(DEFINED PLUGIN_ICONS)
+    # TODO: This loses a possible directory structure of the input files
+    install(FILES ${PLUGIN_ICONS}
+      DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/icons
+    )
+  endif()
 endfunction()
