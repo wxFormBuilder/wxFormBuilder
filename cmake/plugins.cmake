cd1548fc8b768164bbcc64a26ec1ea45f4bf5420
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-10-12 22:38:03 +0200

    Add staged build
    Add option to enable staged build, by default it is only enabled for a
    top level project. Update add_plugin() to use the proper directory
    during a staged build. Don't create the resources custom commands and
    targets if staged build is disabled.

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -14,6 +14,27 @@ function(add_plugin PLUGIN_NAME)
     set(PLUGIN_COMPONENTS ${PLUGIN_NAME})
   endif()
 
+  if(WIN32)
+    set(runtimeDestination ${CMAKE_INSTALL_BINDIR}/plugins/${PLUGIN_NAME})
+    set(libraryDestination ${CMAKE_INSTALL_LIBDIR}/plugins/${PLUGIN_NAME})
+    set(archiveDestination ${CMAKE_INSTALL_LIBDIR}/plugins/${PLUGIN_NAME})
+  elseif(APPLE)
+    set(runtimeDestination wxFormBuilder.app/Contents/PlugIns)
+    set(libraryDestination wxFormBuilder.app/Contents/PlugIns)
+    set(archiveDestination wxFormBuilder.app/Contents/PlugIns)
+  else()
+    set(runtimeDestination ${CMAKE_INSTALL_BINDIR}/wxformbuilder)
+    set(libraryDestination ${CMAKE_INSTALL_LIBDIR}/wxformbuilder)
+    set(archiveDestination ${CMAKE_INSTALL_LIBDIR}/wxformbuilder)
+  endif()
+  set(resourceDestination ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME})
+
+  if(WXFB_STAGED_BUILD)
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${WXFB_STAGEDIR}/${runtimeDestination}")
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${WXFB_STAGEDIR}/${libraryDestination}")
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${WXFB_STAGEDIR}/${archiveDestination}")
+  endif()
+
   add_library(wxFormBuilder_${PLUGIN_NAME} MODULE)
   add_library(wxFormBuilder::${PLUGIN_NAME} ALIAS wxFormBuilder_${PLUGIN_NAME})
   set_target_properties(wxFormBuilder_${PLUGIN_NAME} PROPERTIES
@@ -62,7 +83,7 @@ function(add_plugin PLUGIN_NAME)
 
   # TODO: In the final sources layout these files should reside next to the sources
   set(sourceDir "${CMAKE_CURRENT_SOURCE_DIR}/../output/plugins/${PLUGIN_DIRECTORY}/xml")
-  set(destDir "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/xml")
+  set(destDir "${WXFB_STAGEDIR}/${resourceDestination}/xml")
   set(definitionsSource "")
   set(definitionsDest "")
   set(templatesSource "")
@@ -76,43 +97,45 @@ function(add_plugin PLUGIN_NAME)
     endforeach()
   endforeach()
 
-  add_custom_command(OUTPUT ${definitionsDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying component definitions")
-  foreach(source dest IN ZIP_LISTS definitionsSource definitionsDest)
-    add_custom_command(
-      OUTPUT ${definitionsDest}
-      COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
-      DEPENDS "${source}"
-      VERBATIM
-      APPEND
+  if(WXFB_STAGED_BUILD)
+    add_custom_command(OUTPUT ${definitionsDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying component definitions")
+    foreach(source dest IN ZIP_LISTS definitionsSource definitionsDest)
+      add_custom_command(
+        OUTPUT ${definitionsDest}
+        COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
+        DEPENDS "${source}"
+        VERBATIM
+        APPEND
+      )
+    endforeach()
+    add_custom_target(wxFormBuilder_${PLUGIN_NAME}-definitions
+      DEPENDS ${definitionsDest}
     )
-  endforeach()
-  add_custom_target(wxFormBuilder_${PLUGIN_NAME}-definitions
-    DEPENDS ${definitionsDest}
-  )
 
-  add_custom_command(OUTPUT ${templatesDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying code templates")
-  foreach(source dest IN ZIP_LISTS templatesSource templatesDest)
-    add_custom_command(
-      OUTPUT ${templatesDest}
-      COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
-      DEPENDS "${source}"
-      VERBATIM
-      APPEND
+    add_custom_command(OUTPUT ${templatesDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying code templates")
+    foreach(source dest IN ZIP_LISTS templatesSource templatesDest)
+      add_custom_command(
+        OUTPUT ${templatesDest}
+        COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
+        DEPENDS "${source}"
+        VERBATIM
+        APPEND
+      )
+    endforeach()
+    add_custom_target(wxFormBuilder_${PLUGIN_NAME}-templates
+      DEPENDS ${templatesDest}
     )
-  endforeach()
-  add_custom_target(wxFormBuilder_${PLUGIN_NAME}-templates
-    DEPENDS ${templatesDest}
-  )
 
-  add_dependencies(wxFormBuilder_${PLUGIN_NAME}
-    wxFormBuilder_${PLUGIN_NAME}-definitions
-    wxFormBuilder_${PLUGIN_NAME}-templates
-  )
+    add_dependencies(wxFormBuilder_${PLUGIN_NAME}
+      wxFormBuilder_${PLUGIN_NAME}-definitions
+      wxFormBuilder_${PLUGIN_NAME}-templates
+    )
+  endif()
 
   if(DEFINED PLUGIN_ICONS)
     # TODO: In the final sources layout these files should reside next to the sources  
     set(sourceDir "${CMAKE_CURRENT_SOURCE_DIR}/../output/plugins/${PLUGIN_DIRECTORY}/icons")
-    set(destDir "${CMAKE_CURRENT_BINARY_DIR}/${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/icons")
+    set(destDir "${WXFB_STAGEDIR}/${resourceDestination}/icons")
     set(iconsSource "")
     set(iconsDest "")
     foreach(icon IN LISTS PLUGIN_ICONS)
@@ -120,63 +143,48 @@ function(add_plugin PLUGIN_NAME)
       list(APPEND iconsDest "${destDir}/${icon}")
     endforeach()
 
-    add_custom_command(OUTPUT ${iconsDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying icons")
-    foreach(source dest IN ZIP_LISTS iconsSource iconsDest)
-      add_custom_command(
-        OUTPUT ${iconsDest}
-        COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
-        DEPENDS "${source}"
-        VERBATIM
-        APPEND
+    if(WXFB_STAGED_BUILD)
+      add_custom_command(OUTPUT ${iconsDest} COMMENT "wxFormBuilder_${PLUGIN_NAME}: Copying icons")
+      foreach(source dest IN ZIP_LISTS iconsSource iconsDest)
+        add_custom_command(
+          OUTPUT ${iconsDest}
+          COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${source}" "${dest}"
+          DEPENDS "${source}"
+          VERBATIM
+          APPEND
+        )
+      endforeach()
+      add_custom_target(wxFormBuilder_${PLUGIN_NAME}-icons
+        DEPENDS ${iconsDest}
       )
-    endforeach()
-    add_custom_target(wxFormBuilder_${PLUGIN_NAME}-icons
-      DEPENDS ${iconsDest}
-    )
 
-    add_dependencies(wxFormBuilder_${PLUGIN_NAME}
-      wxFormBuilder_${PLUGIN_NAME}-icons
-    )
+      add_dependencies(wxFormBuilder_${PLUGIN_NAME}
+        wxFormBuilder_${PLUGIN_NAME}-icons
+      )
+    endif()
   endif()
 
-  if(WIN32)
-    # TODO: Using RUNTIME_DEPENDENCIES results in the dependencies getting installed in the locations
-    #       defined in this command, this is however unwanted because they should get installed in
-    #       the global location. Registering the dependencies in a set and installing them later with
-    #       the standalone command has the desired effect, but why?
-    install(TARGETS wxFormBuilder_${PLUGIN_NAME}
-      RUNTIME_DEPENDENCY_SET wxFormBuilder_deps
-      RUNTIME
-        DESTINATION ${CMAKE_INSTALL_BINDIR}/plugins/${PLUGIN_NAME}
-      LIBRARY
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/plugins/${PLUGIN_NAME}
-    )
-  elseif(APPLE)
-    install(TARGETS wxFormBuilder_${PLUGIN_NAME}
-      RUNTIME
-        DESTINATION wxFormBuilder.app/Contents/PlugIns
-      LIBRARY
-        DESTINATION wxFormBuilder.app/Contents/PlugIns
-    )
-  else()
-    install(TARGETS wxFormBuilder_${PLUGIN_NAME}
-      RUNTIME
-        DESTINATION ${CMAKE_INSTALL_BINDIR}/wxformbuilder
-      LIBRARY
-        DESTINATION ${CMAKE_INSTALL_LIBDIR}/wxformbuilder
-    )
-  endif()
-
+  # TODO: Using RUNTIME_DEPENDENCIES results in the dependencies getting installed in the locations
+  #       defined in this command, this is however unwanted because they should get installed in
+  #       the global location. Registering the dependencies in a set and installing them later with
+  #       the standalone command has the desired effect, but why?
+  install(TARGETS wxFormBuilder_${PLUGIN_NAME}
+    RUNTIME_DEPENDENCY_SET wxFormBuilder_deps
+    RUNTIME
+      DESTINATION ${runtimeDestination}
+    LIBRARY
+      DESTINATION ${libraryDestination}
+  )
   # TODO: In the final sources layout these files should reside next to the sources
   install(FILES ${definitionsSource}
-    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/xml
+    DESTINATION ${resourceDestination}/xml
   )
   install(FILES ${templatesSource}
-    DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}/xml
+    DESTINATION ${resourceDestination}/xml
   )
   if(DEFINED PLUGIN_ICONS)
     install(DIRECTORY ../output/plugins/${PLUGIN_DIRECTORY}/icons
-      DESTINATION ${CMAKE_INSTALL_DATADIR}/plugins/${PLUGIN_NAME}
+      DESTINATION ${resourceDestination}
     )
   endif()
 endfunction()
