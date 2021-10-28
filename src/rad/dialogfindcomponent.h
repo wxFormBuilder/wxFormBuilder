2d05b0c9caa5481d9410465fef042bcc50335ba8
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2021-10-28 16:14:50 +0200

    Fix typo in copyright header

--- src/rad/dialogfindcomponent.h
+++ src/rad/dialogfindcomponent.h
@@ -18,7 +18,7 @@
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 //
-// DialogFindComponent writen by
+// DialogFindComponent written by
 //   Michał Łukowski - michal.lukowski@gmail.com
 //
 ///////////////////////////////////////////////////////////////////////////////
