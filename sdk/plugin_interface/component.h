f8c315bbc97a847eefbdd8378ab11235b90fcec6
Steffen Olszewski <steffen.olszewski@gero-mess.de>
2022-04-15 19:17:27 +0200

    Add or update documentation
    Use proper Doxygen syntax.

--- sdk/plugin_interface/component.h
+++ sdk/plugin_interface/component.h
@@ -41,9 +41,11 @@ class Element;
 class IComponentLibrary;
 
 
-// Plugins interface
-// The point is to provide an interface for accessing the object's properties
-// from the plugin itself, in a safe way.
+/**
+ * @brief Object interface for plugins
+ *
+ * This interface is used by plugin components to query properties for the objects they create.
+ */
 class IObject
 {
 public:
@@ -71,151 +73,277 @@ public:
 };
 
 
-// Used to identify wxObject* that must be manually deleted
+/**
+ * @brief Placeholder object for objects not part of a wxWindow hierarchy
+ *
+ * Plugin components must always create an object, even if they create non-visual elements.
+ * Objects of this class must be returned for such elements.
+ */
 class wxNoObject : public wxObject
 {
 };
 
 
 /**
-Interface to the "Manager" class in the application.
-Essentially a collection of utility functions that take a wxObject* and do something useful.
-*/
+ * @brief Manager interface for plugins
+ *
+ * This interface is used by plugin components to interact with the main application.
+ */
 class IManager
 {
 public:
     virtual ~IManager() = default;
 
     /**
-    Get the corresponding object interface pointer for the object.
-    This allows easy read only access to properties.
-    */
+     * @brief Get the object interface of the object
+     *
+     * @param wxobject Object
+     * @return Object interface
+     */
     virtual IObject* GetIObject(wxObject* wxobject) const = 0;
 
     /**
-    Get the count of the children of this object.
-    */
+     * @brief Get the count of child objects
+     *
+     * @param wxobject Object
+     * @return Number of child objects
+     */
     virtual std::size_t GetChildCount(wxObject* wxobject) const = 0;
     /**
-    Get a child of the object.
-    @param childIndex Index of the child to get.
-    */
+     * @brief Get a child object
+     *
+     * @param wxobject Object
+     * @param childIndex Child index
+     * @return Child object at childIndex
+     */
     virtual wxObject* GetChild(wxObject* wxobject, std::size_t childIndex) const = 0;
 
     /**
-    Get the parent of the object.
-    */
+     * @brief Get the parent object
+     *
+     * @param wxobject Object
+     * @return Parent object of object
+     */
     virtual wxObject* GetParent(wxObject* wxobject) const = 0;
     /**
-    Get the IObject interface to the parent of the object.
-    */
+     * @brief Get the object interface of the parent object
+     *
+     * @param wxobject Object
+     * @return Object interface of parent object of object
+     */
     virtual IObject* GetIParent(wxObject* wxobject) const = 0;
 
     /**
-    Select the object in the object tree
-    Returns true if selection changed, false if already selected
-    */
+     * @brief Select the object in the object tree
+     *
+     * @param wxobject The object to select
+     * @return True, if the selection did change, false, if the object was already selected
+     */
     virtual bool SelectObject(wxObject* wxobject) = 0;
-
     /**
-    Modify a property of the object.
-    @param property The name of the property to modify.
-    @param value The new value for the property.
-    @param allowUndo If true, the property change will be placed into the undo stack, if false it will be modified
-    silently.
-    */
+     * @brief Modify the property of the object
+     *
+     * @param wxobject Object to modify
+     * @param property Property to modify
+     * @param value New value of the property
+     * @param allowUndo If true, the property change will be placed into the undo stack, otherwise the change will be silent
+     */
     virtual void ModifyProperty(wxObject* wxobject, const wxString& property, const wxString& value, bool allowUndo = true) = 0;
 
-    // used so the wxNoObjects are both created and destroyed in the application
+    /**
+     * @brief Create a wxNoObject
+     *
+     * TODO: This shall ensure that these objects are created in the main application and the dynamic cast to test for them
+     *       does work there, but is this really necessary? All other objects are created inside the plugin and deleted
+     *       by wxWidgets in the main application.
+     *
+     * @return A wxNoObject
+     */
     virtual wxNoObject* NewNoObject() = 0;
 };
 
 
 /**
- * Component Interface
+ * @brief Base interface of a plugin component
+ *
+ * A plugin component implements the functionality to create objects of a specific type for usage by the main application.
  */
 class IComponent
 {
 public:
+    /**
+     * @brief Type of the component
+     */
     enum class Type {
+        /// Non-visual element, won't be inserted into a wxWindow hierarchy
         Abstract = 0,
+        /// Visual element that will be inserted into a wxWindow hierarchy
         Window,
+        /// Sizer element
         Sizer,
     };
 
 public:
     virtual ~IComponent() = default;
 
+    /**
+     * @brief Get the component library of this object
+     *
+     * @return The component library of this object, this is never nullptr
+     */
     virtual IComponentLibrary* GetLibrary() const = 0;
-    
+
+    /**
+     * @return Type of this component
+     */
     virtual Type GetComponentType() const = 0;
 
     /**
-     * Create an instance of the wxObject and return a pointer
+     * @brief Create a new object
+     *
+     * @param obj Properties of the object to create
+     * @param parent Parent object of the object to create
+     * @return The created object
      */
     virtual wxObject* Create(IObject* obj, wxObject* parent) = 0;
     /**
-     * Cleanup (do the reverse of Create)
+     * @brief Perform additional cleanup actions before deleting the object
+     *
+     * This function gets called just before the object is about to be deleted
+     * and allows to perform additional cleanup actions.
+     *
+     * @param wxobject Object created by Create(IObject*, wxObject*)
      */
     virtual void Cleanup(wxObject* wxobject) = 0;
 
     /**
-     * Allows components to do something after they have been created.
-     * For example, Abstract components like NotebookPage and SizerItem can
-     * add the actual widget to the Notebook or sizer.
+     * @brief Callback function executed after the creation of the object
      *
-     * @param wxobject The object which was just created.
-     * @param wxparent The wxWidgets parent - the wxObject that the created object was added to.
+     * This function gets called after the object has been created and registered into
+     * the internal data structure. This function can be used to perform additional steps,
+     * e.g. abstract components like notebookpage or sizeritem can add the created widget to
+     * the notebook or sizer.
+     *
+     * @param wxobject The object that was created
+     * @param wxparent The parent object of the created object
      */
     virtual void OnCreated(wxObject* wxobject, wxWindow* wxparent) = 0;
     /**
-     * Allows components to respond when selected in object tree.
-     * For example, when a wxNotebook's page is selected, it can switch to that page
+     * @brief Callback function executed after the object has been selected in the object tree
+     *
+     * This function can be used to perform additional steps after the object has been selected,
+     * e.g. after selecting a notebookpage the notebook could switch to that page.
+     *
+     * @param wxobject The selected object
      */
     virtual void OnSelected(wxObject* wxobject) = 0;
 
     /**
-     * Export the object to an XRC node
+     * @brief Export object properties into a XRC node
+     *
+     * @param obj Object properties
+     * @return The created XRC node, the caller gets ownership of the object
      */
     virtual ticpp::Element* ExportToXrc(IObject* obj) = 0;
     /**
-     * Converts from an XRC element to a wxFormBuilder project file XML element
+     * @brief Import object properties from a XRC node
+     *
+     * The properties are converted into a XML node of a wxFormBuilder project file
+     *
+     * @param xrcObj XRC node
+     * @return wxFormBuilder project file XML node, the caller gets ownership of the object
      */
     virtual ticpp::Element* ImportFromXrc(ticpp::Element* xrcObj) = 0;
 };
 
 
-// Interface which intends to contain all the components for a plugin
-// This is an abstract class and it'll be the object that the DLL will export.
+/**
+ * @brief Component library interface of a plugin
+ *
+ * This interface is used by the plugin to register all its components.
+ * The main application uses this interface to access the components of the plugin.
+ */
 class IComponentLibrary
 {
 public:
     virtual ~IComponentLibrary() = default;
 
-    // Used by the plugin for registering components and macros
+    /**
+     * @brief Register a component
+     *
+     * @param name Name of the component
+     * @param component Component implementation, ownership is claimed
+     */
     virtual void RegisterComponent(const wxString& name, IComponent* component) = 0;
+    /**
+     * @brief Register a macro
+     *
+     * @param macro Symbolic name of the macro
+     * @param value Value of the macro
+     */
     virtual void RegisterMacro(const wxString& macro, int value) = 0;
+    /**
+     * @brief Register a macro synonymous
+     *
+     * @param synonymous Synonymous name of the macro
+     * @param macro Symbolic name of the macro
+     */
     virtual void RegisterSynonymous(const wxString& synonymous, const wxString& macro) = 0;
 
+    /**
+     * @brief Get the used manager object
+     *
+     * @return Manager object, this is never nullptr
+     */
     virtual IManager* GetManager() const = 0;
+    /**
+     * @brief Replace a macro synonymous by the macro itself
+     *
+     * @param synonymous Synonymous name of the macro
+     * @param replaced If not nullptr, set to true if a replacement was done, otherwise to false
+     * @return The macro name or synonymous if the given value is not a synonymous of a macro
+     */
     virtual wxString ReplaceSynonymous(const wxString& synonymous, bool* replaced = nullptr) const = 0;
 
-    // Used by wxFormBuilder for recovering components and macros
+    /**
+     * @brief Get all registered components
+     *
+     * @return All registered comonents, the objects are owned by this instance and must not be deleted
+     */
     virtual std::vector<std::pair<wxString, IComponent*>> GetComponents() const = 0;
+    /**
+     * @brief Get all registered macros
+     *
+     * @return All registered macros
+     */
     virtual std::vector<std::pair<wxString, int>> GetMacros() const = 0;
 };
 
 
+// The component library interface functions are part of a statically linked library that
+// gets linked into a shared library. The implementation itself is done directly inside the
+// shared library though. Since the shared library is loaded dynamically and the functions
+// are resolved dynamically, this very limited export specification is sufficient.
 #ifdef BUILD_DLL
     #define DLL_FUNC extern "C" WXEXPORT
 #else
     #define DLL_FUNC extern "C"
 #endif
 
-// Function that the application calls to get the library
+/**
+ * @brief Create a component library object
+ *
+ * Creates a new instance of the component library of the plugin using the given manager object.
+ *
+ * @param manager Manager object used by the component library
+ * @return The component library object
+ */
 DLL_FUNC IComponentLibrary* CreateComponentLibrary(IManager* manager);
-
-// Function that the application calls to free the library
+/**
+ * @brief Deletes a component library object
+ *
+ * @param lib The component library object, it must have been created by this plugin
+ */
 DLL_FUNC void FreeComponentLibrary(IComponentLibrary* lib);
 
 #endif  // SDK_PLUGIN_INTERFACE_COMPONENT_H
