8e6bc081a762843b319ecd9801b0ad00cef3251c
Steffen Olszewski <steffen.o.dev@gmail.com>
2025-09-06 12:38:47 +0200

    WIP: Search for versioned wxrc too

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -200,7 +200,7 @@ function(wxfb_create_target_wxwidgets)
   endif()
   if(NOT wxWidgets_wxrc_EXECUTABLE)
     # Config-Mode does not search for wxrc, assume the environment is properly setup and search for it ourself
-    find_program(wxWidgets_wxrc_EXECUTABLE NAMES wxrc REQUIRED)
+    find_program(wxWidgets_wxrc_EXECUTABLE NAMES wxrc "wxrc-${wxWidgets_VERSION_MAJOR}.${wxWidgets_VERSION_MINOR}" REQUIRED)
     mark_as_advanced(wxWidgets_wxrc_EXECUTABLE)
   endif()
 
